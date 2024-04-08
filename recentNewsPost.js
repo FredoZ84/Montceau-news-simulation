335c703d317ab26cf99e1a3770799e4ca8908a04
FredoZ84 <frederic.zahoui@gmail.com>
2024-04-08 15:13:02 +0200

    Réajustement media querries

--- recentNewsPost.js
+++ recentNewsPost.js
@@ -1,5 +1,25 @@
 function reorganization() {
-    //attribution d'une balise span 
+    //attribution d'une div classe primaryBlock
+    let slidhead = document.querySelector(".slidhead")
+    let titlePrimaryBlock = document.querySelectorAll(".titlePrimaryBlock")
+    let triangle = document.querySelectorAll(".triangle")
+    let imgPrimaryBlock = document.querySelectorAll(".imgPrimaryBlock")
+
+    for (let index = 0; index < titlePrimaryBlock.length; index++) {
+        //titlePrimaryBlock[index]
+        
+        let primaryBlock = document.createElement("div")
+        primaryBlock.classList.add("primaryBlock")
+        slidhead.appendChild(primaryBlock)
+
+        // Dé^placement
+        primaryBlock.appendChild(titlePrimaryBlock[index])
+        primaryBlock.appendChild(triangle[index])
+        primaryBlock.appendChild(imgPrimaryBlock[index])
+
+
+    }
+    //attribution d'une balise span au p
 
     let paragraphs = document.querySelectorAll(".justContent p")
   
