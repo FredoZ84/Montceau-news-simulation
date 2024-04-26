1e5e28fe2edfc1bbf6738a6b0e7c914ddaa05f99
FredoZ84 <frederic.zahoui@gmail.com>
2024-04-26 06:26:43 +0200

    Nouvelle adaptation mobile

--- header.js
+++ header.js
@@ -1,7 +1,20 @@
 function reconfiguration() {
+    let body = document.querySelector("body")
+    let preHeader = document.getElementById("pre-header")
+    let firstChild = document.getElementById("firstChild")
+
+
+    
+    // insertion d'un header en position fixe
+    let header = document.createElement("header")
+    body.insertBefore(header,preHeader)
+
+    header.appendChild(preHeader)
+    header.appendChild(firstChild)
+    
     let headerTop = document.getElementById("header_top")
 
-    let logo = document.querySelectorAll("#header_top a")
+    //let logo = document.querySelector("#header_top a")
     let searchBlock = document.getElementById("search_block")
     let wBlock = document.getElementById("wblock")
 
@@ -9,9 +22,9 @@ function reconfiguration() {
     otherBlocks.id = "other_blocks"
     headerTop.appendChild(otherBlocks)
 
+    // Déplacement des éléments
     otherBlocks.appendChild(searchBlock)
     otherBlocks.appendChild(wBlock)
-
 }
 
 reconfiguration()
